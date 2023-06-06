434474c0d21dfa2b066d898b5543f5f280455b9f
Autrin <autrinhakimi@gmail.com>
2023-06-05 21:30:52 -0500

    Update Animal.java

--- src/Animal.java
+++ src/Animal.java
@@ -1,1 +1,33 @@
+package wildlife
 
+/**
+ * @author Autrin Hakimi
+ * This class is to be extended by the Badger, Fox, and Rabbit classes. 
+ */
+public abstract class Animal extends Living implements MyAge
+{
+	protected int age;   // age of the animal 
+
+	/**
+	 * the constructor of animal
+	 * @author Autrin Hakimi
+	 * @param p
+	 * @param r
+	 * @param c
+	 * @param a
+	 */
+	public Animal(Plain p, int r, int c, int a) {
+		super(p, r, c);
+		age = a;
+	}
+	@Override
+	/**
+	 * 
+	 * @return age of the animal 
+	 */
+	public int myAge()
+	{
+		// TODO 
+		return age; 
+	}
+}
